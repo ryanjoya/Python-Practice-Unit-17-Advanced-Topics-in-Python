3f1238124d873f8c095e9057af6b80146d0fab41
Ryan Joya <ryanjoya@users.noreply.github.com>
2015-08-18 23:53:37 -0500

    Create 17.3 The 'in' Operator.py

---
+++ 17.3 The 'in' Operator.py
@@ -0,0 +1,12 @@
+my_dict = {
+    "Name": "Unreasonable",
+    "Age": 37,
+    "Class": "Rawrior"
+}
+
+print my_dict.keys()
+
+print my_dict.values()
+
+for key in my_dict:
+    print key, my_dict[key]
